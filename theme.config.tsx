33092ea85cea5f2c241450c86db6847797636df6
Firgrep <awdeegrays@gmail.com>
2023-09-12 11:04:19 +0200

    building menus and routes for testing

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,7 +1,31 @@
-export default {
+import { useRouter } from "next/router";
+import { type DocsThemeConfig, useConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
     logo: <span>sPhil Nextra</span>,
     project: {
-      link: 'https://github.com/shuding/nextra'
-    }
-    // ... other theme options
+      	link: 'https://github.com/shuding/nextra'
+    },
+    useNextSeoProps: function SEO() {
+		const router = useRouter();
+		const { frontMatter } = useConfig();
+
+		let section = "sPhil";
+		if (router?.pathname.startsWith("/hegel")) {
+			section = "Hegel";
+		}
+		if (router?.pathname.startsWith("/kant")) {
+			section = "Kant";
+		}
+
+		const defaultTitle = frontMatter.overrideTitle || section;
+
+		return {
+			description: frontMatter.description,
+			defaultTitle,
+			titleTemplate: `%s – ${section}`,
+		}
+	}
 }
+
+export default config;
