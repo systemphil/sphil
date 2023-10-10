6bcdb378c6d54220d16358f181843cdde5fbcf9e
Firgrep <awdeegrays@gmail.com>
2023-10-10 16:10:40 +0200

    refactor: re-allocated favicon files, removed description from index files, feat: added head props

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,6 +4,8 @@ import Footer from "@/components/Footer";
 import { useRouter } from "next/router";
 import { type DocsThemeConfig, useConfig } from "nextra-theme-docs";
 
+const SITE_ROOT = "https://sphil-test.ey.r.appspot.com/";
+
 const config: DocsThemeConfig = {
 	docsRepositoryBase: "https://github.com/Firgrep/sphil/tree/main", // root for every edit link
 	editLink: {
@@ -11,6 +13,63 @@ const config: DocsThemeConfig = {
 	},
 	footer: {
 		component: Footer,
+	},
+	head: function Head() {
+		const router = useRouter();
+		const { frontMatter } = useConfig();
+		const systemTheme = "light";
+		const fullUrl =
+		  	router.asPath === "/" ? SITE_ROOT : `${SITE_ROOT}${router.asPath}`;
+	
+		return (
+			<>
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href={`/images/favicon-${systemTheme}/apple-touch-icon.png`}
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href={`/images/favicon-${systemTheme}/favicon-32x32.png`}
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href={`/images/favicon-${systemTheme}/favicon-16x16.png`}
+				/>
+				<link
+					rel="mask-icon"
+					href={`/images/favicon-${systemTheme}/safari-pinned-tab.svg`}
+					color="#FFFFFF"
+				/>
+				<link
+					rel="shortcut icon"
+					href={`/images/favicon-${systemTheme}/favicon.ico`}
+				/>
+				<meta name="msapplication-TileColor" content="#FFFFFF" />
+				<meta name="theme-color" content="#FFF" />
+				<meta property="og:type" content="website" />
+				<meta property="og:url" content={fullUrl} />
+				<link rel="canonical" href={fullUrl} />
+				<meta name="image" property="og:image" content={`${SITE_ROOT}/og-image.png`} />
+				<meta property="og:locale" content="en_IE" />
+				<meta property="og:site_name" content="sPhil" />
+				<meta
+					name="description"
+					property="og:description"
+					content={frontMatter.description || 'Where Philosophy Meets Open Collaboration'}
+				/>
+				<meta 
+					name="title" 
+					property="og:title" 
+					content={frontMatter.overrideTitle || frontMatter.title || "sPhil"}
+				/>
+			</>
+		);
 	},
     logo: NavbarHeader,
 	logoLink: false,
