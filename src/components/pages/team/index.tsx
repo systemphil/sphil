b3b969bfb1b710ae7424bee779115c86565bc2e4
Firgrep <awdeegrays@gmail.com>
2023-12-06 19:55:27 +0100

    fix: adjusting responsiveness for sm screens

--- src/components/pages/team/index.tsx
+++ src/components/pages/team/index.tsx
@@ -6,7 +6,7 @@ const TeamPage = () => {
     return(
         <div className="flex flex-col container justify-center w-full items-center">
             <h1 className="text-4xl font-bold my-16">Meet Our Team</h1>
-            <div className="flex gap-16 flex-wrap mb-60">
+            <div className="flex justify-center gap-16 flex-wrap mb-60">
                 <CardTeamMember 
                     name="Filip Niklas" 
                     title="Tech Lead" 
