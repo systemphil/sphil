a58ba2e8800a2f36561e398f39171137c633f479
Filip Niklas <118931755+Firgrep@users.noreply.github.com>
2024-07-27 15:15:21 +0200

    fix: small license txt

--- src/components/License.tsx
+++ src/components/License.tsx
@@ -16,9 +16,9 @@ export const License = () => {
                 <span className="tooltip rounded shadow-lg p-1 bg-gray-100  dark:bg-slate-900 text-center">
                     You&apos;re allowed to freely share, remix, adapt, and build
                     upon the work non-commercially, as long as credit is given
-                    to the author(s) and new creations are licensed under
-                    identical terms. Click the link below to view the full
-                    license.
+                    to the author(s), a link to the source is provided and new
+                    creations are licensed under identical terms. Click the link
+                    below to view the full license.
                 </span>
             </div>
             <a
