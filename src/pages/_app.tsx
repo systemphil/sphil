62200c6bbe387c68b8640bf5c024bb34b40d7715
Filip Niklas <awdeegrays@gmail.com>
2024-10-22 18:25:12 +0200

    chore: convert meta json to ts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
 import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 import Analytics from "@/components/Analytics";
 import "../styles/overrides.css";
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
     return (
         <>
             <Component {...pageProps} />
---
+++ src/pages/hegel/reference/_meta.ts
@@ -0,0 +1,21 @@
+export default {
+    index: {
+        title: "",
+    },
+    "---": {
+        type: "separator",
+        title: "The Science of Logic",
+    },
+    being: {
+        title: "Being",
+    },
+    nothing: {
+        title: "Nothing",
+    },
+    becoming: {
+        title: "Becoming",
+    },
+    "mechanical-object": {
+        title: "Mechanical Object",
+    },
+};
