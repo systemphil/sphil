579fa2b61dbb51fbc67c54dd0d64628d642499dc
jmielbrecht <jordan.mielbrecht@gmail.com>
2024-12-30 17:45:48 -0800

    rename export, mark with 'use client'

--- lib/components/ui/ContributionModal.tsx
+++ lib/components/ui/ContributionModal.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
+'use client'
+import React, { useState } from 'react';
 import { Modal, useTheme } from '@mui/material';
 
-const RedirectModal = () => {
+const ContributionModal = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleRedirect = () => {
@@ -32,4 +33,4 @@ const RedirectModal = () => {
   );
 };
 
-export default RedirectModal;
+export default ContributionModal;
