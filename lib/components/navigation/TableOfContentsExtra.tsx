77347b56217f781840fb6cc691307f1a73ac73e5
Filip Niklas <awdeegrays@gmail.com>
2025-01-06 18:26:06 +0100

    chore: remove comment

--- lib/components/navigation/TableOfContentsExtra.tsx
+++ lib/components/navigation/TableOfContentsExtra.tsx
@@ -12,7 +12,6 @@ export function TableOfContentsExtra() {
 function LinkToDiscussion() {
     return (
         <a
-            // TODO fix classes, nextra no longer uses nx- prefix
             className="focus-visible:nextra-focus text-xs font-medium text-gray-600 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-200 contrast-more:text-gray-700 contrast-more:dark:text-gray-100"
             target="_blank"
             href="https://github.com/systemphil/sphil/discussions"
