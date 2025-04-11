a97f361189e50c42a0ec27da25b1e68d2015b1d3
Filip Niklas <awdeegrays@gmail.com>
2025-04-11 14:57:44 +0200

    test: fixed at breakpoints

--- features/courses/components/LessonFrontPage.tsx
+++ features/courses/components/LessonFrontPage.tsx
@@ -56,17 +56,19 @@ export async function LessonFrontPage({ lessonSlug }: { lessonSlug: string }) {
                 </div>
             </div>
             <div className="md:col-span-1 md:p-2 md:order-1">
-                <div className="flex flex-col justify-start">
-                    <Link
-                        href={`/symposia/courses/${lessonData.course.slug}`}
-                        className="text-base self-center md:self-start text-primary dark:text-gray-500 opacity-70 transition hover:opacity-100 p-2"
-                    >
-                        {`<- Back to ${lessonData.course.name}`}
-                    </Link>
-                    <TableOfLessons
-                        lessons={lessonData.course.lessons}
-                        courseSlug={lessonData.course.slug}
-                    />
+                <div className="xl:fixed max-w-[300px]">
+                    <div className="flex flex-col justify-start">
+                        <Link
+                            href={`/symposia/courses/${lessonData.course.slug}`}
+                            className="text-base self-center md:self-start text-primary dark:text-gray-500 opacity-70 transition hover:opacity-100 p-2"
+                        >
+                            {`<- Back to ${lessonData.course.name}`}
+                        </Link>
+                        <TableOfLessons
+                            lessons={lessonData.course.lessons}
+                            courseSlug={lessonData.course.slug}
+                        />
+                    </div>
                 </div>
             </div>
             <div className="w-full px-2 md:px-0 md:w-auto md:col-span-4 md:m-4 md:order-3 lg:col-span-2">
