9c28166ec04cc20b9a51588bf4feea6f49b215da
Filip Niklas <awdeegrays@gmail.com>
2025-09-14 15:07:29 +0200

    fix: unnecessary textfield slot that causes incorrent props passage

--- features/courses/components/forms/DateTimeInput.tsx
+++ features/courses/components/forms/DateTimeInput.tsx
@@ -5,7 +5,6 @@ import { Field } from "./Field";
 import { Label } from "./Label";
 import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { TextField } from "@mui/material";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import dayjs from "dayjs";
 
@@ -31,15 +30,6 @@ export const DateTimeInput = ({ name, label, options }: Props) => {
                         <DateTimePicker
                             value={field.value ? dayjs(field.value) : null}
                             onChange={(date) => field.onChange(date)}
-                            slots={{ textField: TextField }}
-                            slotProps={{
-                                textField: {
-                                    fullWidth: true,
-                                    error: !!errors[name],
-                                    className: "bg-white rounded-sm mb-2",
-                                    size: "small",
-                                },
-                            }}
                         />
                     )}
                 />
