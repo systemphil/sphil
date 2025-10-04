b1b29aa882172110b9903eaff0eda717e82c1d79
Filip Niklas <awdeegrays@gmail.com>
2025-10-04 18:43:10 +0200

    chore: remove dead import

--- features/marketing/components/Reviews.tsx
+++ features/marketing/components/Reviews.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Image from "next/image";
 import ReviewsData from "../data/reviews.json";
 import { useEffect, useState } from "react";
 import { CardShell } from "lib/components/ui/CardShell";
