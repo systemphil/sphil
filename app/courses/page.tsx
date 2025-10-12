078099003a9e31aec668987040e3d016dbd61938
Filip Niklas <awdeegrays@gmail.com>
2025-10-12 20:37:55 +0200

    chore: fix more seo, delete (public) grouping

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -4,9 +4,28 @@ import { Loading } from "lib/components/animations/Loading";
 import { Heading } from "lib/components/ui/Heading";
 import { PageWrapper } from "lib/components/ui/PageWrapper";
 import { dbGetAllPublishedCourses } from "lib/database/dbFuncs";
+import { Metadata } from "next";
 import { Suspense } from "react";
 
-export const metadata = {};
+const title = "sPhil Courses";
+const description =
+    "Guided studies of the world's foundational philosophical and literary texts designed for close reading and lasting understanding.";
+
+export const metadata: Metadata = {
+    title: title,
+    description,
+    openGraph: {
+        title: title,
+        description,
+        type: "website",
+        url: "https://sphil.xyz/courses",
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: title,
+        description,
+    },
+};
 
 export async function generateStaticParams() {
     const courses = await dbGetAllPublishedCourses();
--- lib/components/navigation/NavbarHeader.tsx
+++ lib/components/navigation/NavbarHeader.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { LogoOwl } from "../LogoOwl";
 import { LogoAnimated } from "../LogoAnimated";
-import { SiteSwitcher } from "./SiteSwitcher";
+import { TITLE } from "lib/config/consts";
 
 export function NavbarHeader() {
     return (
@@ -9,19 +9,15 @@ export function NavbarHeader() {
             <h2 id="navbar-heading" className="sr-only">
                 Navbar heading
             </h2>
-            <Link
-                href="/"
-                title="Why dost thy hand linger when thou canst but press and enter... Neith's Owl awaits thee"
-                className="flex items-center gap-4"
-            >
+            <Link href="/" title={TITLE} className="flex items-center gap-4">
                 <LogoOwl />
                 <div className="hidden min-[350px]:flex">
                     <LogoAnimated />
                 </div>
             </Link>
-            <div className="hidden xl:flex absolute left-1/2 transform -translate-x-1/2 lg:ml-32 md:relative md:left-0 md:transform-none">
+            {/* <div className="hidden xl:flex absolute left-1/2 transform -translate-x-1/2 lg:ml-32 md:relative md:left-0 md:transform-none">
                 <SiteSwitcher />
-            </div>
+            </div> */}
         </>
     );
 }
